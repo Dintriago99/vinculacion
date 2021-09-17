4ce16ae2dd4a0892ed636da14179e494b2cd09a7
Darlin <dintriago9654@utm.edu.ec>
2021-09-16 22:42:14 -0500

    inicio deploy a heroku

---
+++ clinica/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'vinculacion',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': 3306,
+        }
+    }
+
+
